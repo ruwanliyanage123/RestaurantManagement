9a77854af57a30c6ee459000adca3568477b629f
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-01-30 11:41:09 +0530

    make sign in snd sign up buttons

--- app/src/main/java/com/ruwan/restaurantmanagement/MainActivity.java
+++ app/src/main/java/com/ruwan/restaurantmanagement/MainActivity.java
@@ -2,12 +2,35 @@ package com.ruwan.restaurantmanagement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-//for test commit
+
+    Button signIn, signUp;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        signIn = (Button)findViewById(R.id.signIn);
+        signUp = (Button)findViewById(R.id.signUp);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
