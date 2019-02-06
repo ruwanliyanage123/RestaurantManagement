9442c5a769e358b805209b119ec3d8cb0f2c3a19
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2019-02-06 19:00:11 +0530

    all changes before to add layout

--- app/src/main/java/com/ruwan/restaurantmanagement/FoodDetails.java
+++ app/src/main/java/com/ruwan/restaurantmanagement/FoodDetails.java
@@ -7,9 +7,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DataSnapshot;
@@ -17,7 +19,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.ruwan.restaurantmanagement.Database.Database;
 import com.ruwan.restaurantmanagement.Model.Food;
+import com.ruwan.restaurantmanagement.Model.Order;
 import com.ruwan.restaurantmanagement.ViewHolder.FoodViewHolder;
 import com.squareup.picasso.Picasso;
 
@@ -34,6 +38,7 @@ import com.squareup.picasso.Picasso;
 
     FirebaseDatabase database;
     DatabaseReference foods;
+    Food currentFood;
     String foodId = "";
     FirebaseRecyclerAdapter<Food,FoodViewHolder> adapter;
 
@@ -51,6 +56,23 @@ import com.squareup.picasso.Picasso;
         //init view
         numberButton = (Button) findViewById(R.id.number_button);
         btnCart  = (FloatingActionButton)findViewById(R.id.btnCart);
+
+        btnCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Database(getBaseContext()).addToCart(new Order(
+                        foodId,
+                        currentFood.getName(),
+                        "1",
+                        currentFood.getPrice(),
+                        currentFood.getDiscount()
+
+                ));
+
+                Toast.makeText(FoodDetails.this,"ADDED TO CART",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         food_description = (TextView)findViewById(R.id.food_description);
         food_name = (TextView)findViewById(R.id.food_name);
         food_price= (TextView)findViewById(R.id.food_price);
@@ -80,15 +102,15 @@ import com.squareup.picasso.Picasso;
         foods.child(foodId).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                Food food = dataSnapshot.getValue(Food.class);
+                currentFood = dataSnapshot.getValue(Food.class);
 
                 //set image
-                Picasso.with(getBaseContext()).load(food.getImage()).into(food_image);
+                Picasso.with(getBaseContext()).load(currentFood.getImage()).into(food_image);
 
-                collapsingToolbarLayout.setTitle(food.getName());
-                food_price.setText(food.getPrice());
-                food_name.setText(food.getName());
-                food_description.setText(food.getDescription());
+                collapsingToolbarLayout.setTitle(currentFood.getName());
+                food_price.setText(currentFood.getPrice());
+                food_name.setText(currentFood.getName());
+                food_description.setText(currentFood.getDescription());
 
             }
 
